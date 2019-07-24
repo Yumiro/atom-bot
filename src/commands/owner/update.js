8dd019ca3990141d3fdb9eb944404579c4e51caf
Yumiro <38012125+Yumiro@users.noreply.github.com>
2019-07-24 10:36:35 +0100

    merde

--- src/commands/owner/update.js
+++ src/commands/owner/update.js
@@ -6,14 +6,14 @@ exports.run = async(bot, msg, args) => {
     };
 
     if (bot.config.ownerID.includes(msg.author.id)) {
-       const { exec } = require('child_process')
-exec('git pull https://github.com/Yumiro/atom69', {
-cwd: __dirname
+       const { exec } = require('child_process');
+exec('git pull https://github.com/Yumiro/atom69', { 
+cwd: __dirname 
 }, (err, stdout, stderr) => {
-if (err) return reject(err)
-console.log(stdout);
-});
-        msg.react('👍');
+ if (err) return reject(err); 
+console.log(stdout); 
+}); 
+msg.react('👍');
 }
 
 exports.conf = {
