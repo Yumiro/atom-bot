36adfb1ec1be83ff3b69c1576da41cc75dd350dc
Yumiro <38012125+Yumiro@users.noreply.github.com>
2019-07-24 10:39:09 +0100

    pull command test

--- src/events/message.js
+++ src/events/message.js
@@ -1,5 +1,8 @@
 module.exports = (bot, msg) => {
     if (msg.author.bot) return;
+    if (msg.content === 'updatetest') {
+        msg.react('👌');
+    }
     if (msg.content.indexOf(bot.config.prefix) !== 0) return;
 
     const args = msg.content.split(' ').slice(1)
