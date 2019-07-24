ee42fd474421004d2d814465576a6573d7ae1562
Yumiro <38012125+Yumiro@users.noreply.github.com>
2019-07-24 01:36:53 +0100

    work???

--- src/commands/owner/update.js
+++ src/commands/owner/update.js
@@ -1,7 +1,27 @@
-const { exec } = require('child_process')
+
+
+exports.run = (bot, msg, args) => {
+    if (!bot.config.ownerID.includes(msg.author.id)) {
+        msg.react('👎');
+    };
+
+    if (bot.config.ownerID.includes(msg.author.id)) {
+       const { exec } = require('child_process')
 exec('git pull https://github.com/Yumiro/atom69', {
 cwd: __dirname
 }, (err, stdout, stderr) => {
 if (err) return reject(err)
 console.log(stdout);
-})
+}) 
+};
+
+exports.conf = {
+    dev: true
+}
+
+exports.help = {
+    name: 'update',
+    category: '🚫 Owner',
+    description: 'Git pull nonsense.',
+    usage: 'update'
+}
