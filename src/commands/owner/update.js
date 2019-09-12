b89be53319ea49f828fa77f66a6d53dce47e6287
Flag <38012125+Yumiro@users.noreply.github.com>
2019-09-12 20:07:51 +0100

    Fix update.js v2

--- src/commands/owner/update.js
+++ src/commands/owner/update.js
@@ -8,7 +8,8 @@ exports.run = async(bot, msg, args) => {
             } else {
                 msg.react('👍');
                 console.log(stdout);
-        };
+            };
+        });
     };
 };
 
