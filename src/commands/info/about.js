e2484c080c82194ad07cf5095c00faea3cd5c71d
Flag <38012125+Yumiro@users.noreply.github.com>
2019-10-22 19:17:06 +0100

    Update about.js

--- src/commands/info/about.js
+++ src/commands/info/about.js
@@ -5,16 +5,15 @@ const req = require('node-superfetch');
 
 exports.run = async (bot, msg, args) => {
 
-    const { body } = await req.get('https://api.github.com/repos/Yumiro/atom69/commits');
-    const link = "https://github.com/Yumiro/atom69/commit/";
+    const { body } = await req.get('https://api.github.com/repos/Yumiro/atom69/commits')
     var about = new MessageEmbed()
         .setAuthor(`Changelog`, msg.guild.iconURL(), `https://discordapp.com/invite/eprUzer`)
         .setColor('TRANSPARENT')
-        .addField(`Commit [${link}${body[0].sha.substring(0, 7)}](${body[0].sha.substring(0, 7)}) (${body[0].commit.author.name})`, `** **`)
-        .addField(`Commit [${link}${body[1].sha.substring(0, 7)}](${body[1].sha.substring(0, 7)}) (${body[1].commit.author.name})`, `** **`)
-        .addField(`Commit [${link}${body[2].sha.substring(0, 7)}](${body[2].sha.substring(0, 7)}) (${body[2].commit.author.name})`, `** **`)
-        .addField(`Commit [${link}${body[3].sha.substring(0, 7)}](${body[3].sha.substring(0, 7)}) (${body[3].commit.author.name})`, `** **`)
-        .addField(`Commit [${link}${body[4].sha.substring(0, 7)}](${body[4].sha.substring(0, 7)}) (${body[4].commit.author.name})`, `** **`)
+        .addField(`Commit \`${body[0].sha.substring(0, 7)}\` (${body[0].commit.author.name})`, `** **`)
+        .addField(`Commit \`${body[1].sha.substring(0, 7)}\` (${body[1].commit.author.name})`, `** **`)
+        .addField(`Commit \`${body[2].sha.substring(0, 7)}\` (${body[2].commit.author.name})`, `** **`)
+        .addField(`Commit \`${body[3].sha.substring(0, 7)}\` (${body[3].commit.author.name})`, `** **`)
+        .addField(`Commit \`${body[4].sha.substring(0, 7)}\` (${body[4].commit.author.name})`, `** **`)
         .addField(`Library`, `Discord.js`)
         .addField(`People`, `Main Developer - ${bot.users.get('458659194707640321').tag}\nDeveloper - ${bot.users.get('621154191192096778').tag}\nCool Boy - ${bot.users.get('593510080528515072').tag}`)
         .setFooter(`${bot.version} | Thanks Oly for the changelog part`, `https://cdn.discordapp.com/attachments/502648889728434176/608398619191803936/space.gif`)
