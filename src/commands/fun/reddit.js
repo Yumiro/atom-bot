50697c361f7d64bf954812b43d7407f7263828c6
Flag <38012125+Yumiro@users.noreply.github.com>
2019-11-02 19:36:29 +0000

    Update reddit.js

--- src/commands/fun/reddit.js
+++ src/commands/fun/reddit.js
@@ -16,15 +16,28 @@ exports.run = async (bot, msg, args) => {
         titles = body.data.children[0].data.title;
         message = body.data.children[0].data.permalink;
     });
-
-    const embed = new MessageEmbed()
-        .setTitle(args[0])
-        .setURL(`https://reddit.com${message}`)
-        .setDescription(titles.replace('amp', ''))
-        .setThumbnail(thumbnails)
-    msg.channel.send({
-        embed
-    });
+    if (args[0]) {
+        if (args[1]) {
+            const embed = new MessageEmbed()
+                .setTitle(args[0])
+                .setURL(`https://reddit.com${message}`)
+                .setDescription(titles.replace('amp', ''))
+                .setThumbnail(thumbnails)
+            msg.channel.send({
+                embed
+            });
+        } else {
+            const embedv2 = new MessageEmbed()
+                .addField(firstUpper(this.help.name), this.help.description, false)
+                .addField('Usage', this.help.usage, true)
+                .addField('Example', this.help.example, true)
+                .setColor('TRANSPARENT')
+                .setFooter(msg.guild.name)
+            msg.channel.send({
+                embedv2
+            });
+        }
+    }
 };
 
 exports.conf = {
@@ -32,10 +45,10 @@ exports.conf = {
 }
 
 exports.help = {
-    aliases: ['qr'],
-    name: 'qrcode',
+    aliases: ['r'],
+    name: 'reddit',
     category: '😂 Fun',
-    description: 'Generates a QR Code.',
-    example: 'qrcode xQc',
-    usage: 'qrcode [text]'
+    description: 'Sends the top posts from a specified subreddit.',
+    example: 'reddit softwaregore year',
+    usage: 'reddit [subreddit] [all|year|month|week|day|hour]'
 }
