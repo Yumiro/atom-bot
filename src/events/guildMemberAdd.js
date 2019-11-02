4e73086c4cfc422cb067f3000837a1002aef2143
Flag <38012125+Yumiro@users.noreply.github.com>
2019-11-02 18:54:58 +0000

    Add member logging (test)

---
+++ src/events/guildMemberAdd.js
@@ -0,0 +1,39 @@
+module.exports = (bot, msg, member) => {
+    const channel = member.guild.channels.find(f => f.id === '602575181990330383');
+    let replies = [
+        `Challenger approaching - ${member} has appeared!`,
+        `It's dangerous to go alone, take ${member}!`,
+        `${member} just arrived. Seems OP - please nerf.`,
+        `Cheers, love! ${member} is here!`,
+        `${member} joined. You must construct additional pylons.`,
+        `${member} just joined. Can I get a heal?`,
+        `A wild ${member} appeared.`,
+        `${member} is here, as the prophecy foretold.`,
+        `${member} is here to kick butt and chew bubblegum. And ${member} is all out of gum.`,
+        `${member} hopped into the server. Kangaroo!!`,
+        `${member} has joined the server! It's super effective!`,
+        `${member} has arrived. Party's over.`,
+        `Welcome ${member}. Leave your weapons by the door.`,
+        `Hello. Is it ${member} you're looking for?`,
+        `Big ${member} showed up!`,
+        `${member} just joined the server - glhf!`,
+        `Welcome, ${member}. We hope you brought pizza.`,
+        `${member} has joined. Stay awhile and listen!`,
+        `Ha! ${member} has joined! You activated my trap card!`,
+        `Ermahgerd. ${member} is here.`,
+        `Welcome, ${member}. We were expecting you ( ͡° ͜ʖ ͡°)`,
+        `${member} just showed up. Hold my beer.`,
+        `Brace yourselves. ${member} just joined the server.`,
+        `It's a bird! It's a plane! Nevermind, it's just ${member}.`,
+        `${member} joined your party.`,
+        `Roses are red, violets are blue, ${member} joined this server with you.`,
+        `${member} just joined. Hide your bananas.`,
+        `Swoooosh. ${member} just landed.`,
+        `${member} just slid into the server.`,
+        `Where's ${member}? In the server!`,
+        `It's ${member}! Praise the sun! \\[T]/`,
+        `Never gonna give ${member} up. Never gonna let ${member} down.`,
+    ];
+    let result = Math.floor(Math.random() * replies.length);
+    channel.send(`${replies[result]}`);
+}
---
+++ src/events/guildMemberRemove.js
@@ -0,0 +1,39 @@
+module.exports = (bot, msg, member) => {
+    const channel = member.guild.channels.find(f => f.id === '602575181990330383');
+    let replies = [
+        `${member} has disappeared - not challenging enough.`,
+        `Looks like you forgot to take ${member} with you.`,
+        `${member} left. They seemed too weak - please buff next time.`,
+        `${member} just left.`,
+        `${member} left. They stole our additional pylons.`,
+        `${member} just left. This is so not epic.`,
+        `A wild ${member} disappeared.`,
+        `${member} is not here anymore, unfortunately.`,
+        `${member} is not here to kick butt and chew bubblegum. They left.`,
+        `${member} left - they weren't a Kangaroo.`,
+        `${member} has left the server. It's not effective.`,
+        `${member} has left. PARTY HARD!!!`,
+        `Goodbye ${member}. Don't forget to take your weapons.`,
+        `Bye. Was it ${member} you were looking for? Probably not, because they just left.`,
+        `${member} left the server. This is so sad, Alexa play Despacito.`,
+        `${member} just left the server - they were unlucky and did not have fun.`,
+        `Goodbye, ${member}. Don't forget to bring pizza next time.`,
+        `${member} has left. You can all go play now.`,
+        `Oh no, ${member} left. You deactivated my trap card.`,
+        `NotLikeThis, ${member} left.`,
+        `Goodbye, ${member} ( ͡ ͡° ͡° ʖ ͡° ͡°)`,
+        `${member} just left. Give me my beer.`,
+        `${member} left. You can stop embracing yourselves.`,
+        `It was either a bird or a plane, because ${member} just left.`,
+        `${member} left your party.`,
+        `Roses are red, violets are blue, ${member} left this server without you.`,
+        `${member} just left. Show your bananas.`,
+        `Swoooop. ${member} just left.`,
+        `${member} just left the server.`,
+        `Where's ${member}? Not in this server anymore.`,
+        `${member} left. Do not praise the sun /[⊥]\\`,
+        `Always gonna give ${member} up. Always gonna let ${member} down, because they left.`,
+    ];
+    let result = Math.floor(Math.random() * replies.length);
+    channel.send(`${replies[result]}`);
+}
