028feb8dbb809aa7691a9f8fe95788be74d966dd
Flag <38012125+Yumiro@users.noreply.github.com>
2019-11-29 15:50:10 +0000

    Attempt to fix reddit.js

--- src/commands/fun/reddit.js
+++ src/commands/fun/reddit.js
@@ -4,36 +4,34 @@ const {
 const req = require('superagent');
 
 exports.run = async (bot, msg, args) => {
-    try {
-        req.get(`https://www.reddit.com/r/${args}.json`).query({
-            limit: 75
-        }).set('User-Agent', 'softwaregore-cli').end((err, res) => {
-            if (!err && res.ok) {
-                var random = Math.floor(Math.random() * (75 - 2 + 1) + 1);
-                var subreddit = res.body.data.children[random].data.title;
-                var url = res.body.data.children[random].data.url;
-                var ups = res.body.data.children[random].data.ups;
-                var downs = res.body.data.children[random].data.downs;
-                const embed = new MessageEmbed()
-                    .setTitle(args)
-                    .setDescription(subreddit)
-                    .setColor('TRANSPARENT')
-                    .setFooter(`${ups} Upvotes • ${downs} Downvotes`)
+    req.get(`https://www.reddit.com/r/${args}.json`).query({
+        limit: 75
+    }).set('User-Agent', 'softwaregore-cli').end((err, res) => {
+        if (!err && res.ok) {
+            var random = Math.floor(Math.random() * (75 - 2 + 1) + 1);
+            var subreddit = res.body.data.children[random].data.title;
+            var url = res.body.data.children[random].data.url;
+            var ups = res.body.data.children[random].data.ups;
+            var downs = res.body.data.children[random].data.downs;
+            const embed = new MessageEmbed()
+                .setTitle(args)
+                .setDescription(subreddit)
+                .setColor('TRANSPARENT')
+                .setFooter(`${ups} Upvotes • ${downs} Downvotes`)
 
-                if (!url) {
-                    embed.setImage(url);
-                }
-
+            if (!subreddit) {
                 msg.channel.send({
                     embed
                 });
+            }
 
-            };
+            msg.channel.send({
+                embed
+            });
 
-        });
-    } catch (err) {
-        message.channel.send('No subreddit found.');
-    }
+        };
+
+    });
 };
 
 exports.conf = {
