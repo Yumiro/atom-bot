465b4f4f270159b8e29753aaf4c1e55ccf19ef72
Flag <38012125+Yumiro@users.noreply.github.com>
2019-12-20 12:27:15 +0000

    One last attempt

--- src/commands/fun/reddit.js
+++ src/commands/fun/reddit.js
@@ -13,7 +13,7 @@ exports.run = async (bot, msg, args) => {
             var url = res.body.data.children[random].data.url;
             var ups = res.body.data.children[random].data.ups;
             var downs = res.body.data.children[random].data.downs;
-            if (!url.endsWith('.png' || '.jpg' || '.jpeg')) {
+            if (!url.includes('.png' || '.jpg' || '.jpeg')) {
                 const embed = new MessageEmbed()
                     .setTitle(args)
                     .setDescription(subreddit)
